29d18fc6ca89ef4d3d72bb93c961965d06d89178
Javi <asuncionjavier96@gmail.com>
2026-08-04 22:56:51 +0200

    feat: Añadimos cors para front y swagger y documentamos schemas db

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 import express from "express";
+import cors from "cors";
+// Rutas
+import indexroutes from "./src/routes/indexroutes.js";
+
+// Documentacion 
+import swaggerUI from "swagger-ui-express";     
+import fs from "node:fs"
+
+// necesarias por type:module
+import { join, dirname  } from "node:path";
+import { fileURLToPath } from "node:url";
+
 
 const app = express()
-
 app.use(express.json());
+app.use(cors());
 
 
-// app.use((req,res,next) =>{
-//     next({statusCode: 500, error: new Error("ruta no encontrada") });
-// });
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const swaggerDocument = JSON.parse (fs.readFileSync(join(__dirname, "./swagger.json"), "utf-8"),
+)
+// Rutas
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/api", indexroutes);
 
-
-app.get("/", (req,res) =>{
-    res.send('Hola desde el servidor')
-});
 // Middleware de error
+app.use((err, req, res, next) =>{
+    console.log("error interceptado", err)
+    const statusCode = err.statusCode || 500;
 
-app.use(({statusCode, error}, req, res, next) =>{
-    console.log("Source not found")
+    const message = err.message || "Ha habido un problema"
     return res.status(statusCode).json({
         success: false,
-        error: error.message
+        message: message
     })
 });
+
+
 export default app;
