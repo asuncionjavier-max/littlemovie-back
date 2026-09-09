26261c60705d56d2dfb918cab573f5ef663a02a2
Javi <asuncionjavier96@gmail.com>
2026-09-09 23:08:25 +0200

    fix

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,99 +1,101 @@
 import cloudinary from "../config/cloudinary.js";
-import prisma from "../lib/prisma.js"
+import prisma from "../lib/prisma.js";
 import { uploadloudinary } from "../utils/uploadCloudinary.js";
 
-export const findAllMovies = async (movieIds) =>{
-    try {
-        return{
-            ok:true,
-            content: await prisma.movies.findMany({
-                orderBy:{id: "asc"},
-                omit:{created_at: true, updated_at: true},
-                where: {id : {in: movieIds}}
-            })
-        };
-    } catch (error) {
-        console.error("Error al traer la lista de peliculas")
-        return{
-            ok:false,
-            content: []
-        };
-    }
+export const findAllMovies = async (movieIds) => {
+  try {
+    return {
+      ok: true,
+      content: await prisma.movies.findMany({
+        orderBy: { id: "asc" },
+        omit: { created_at: true, updated_at: true },
+        where: { id: { in: movieIds } },
+      }),
+    };
+  } catch (error) {
+    console.error("Error al traer la lista de peliculas");
+    return {
+      ok: false,
+      content: [],
+    };
+  }
 };
 
-export const findMovieById = async (id) =>{
-    try {
-        const movie = await prisma.movies.findUnique({where: {id},
-        omit: {created_at: true, updated_at: true}})
-        if(!movie) return {
-            ok: false,
-            message: "La pelicula que buscas no existe"
-        }
-        
-        return{
-            ok: true,
-            content: movie
-        } 
-        
-    } catch (error) {
-        return{
-            ok: false,
-            message: "error al consultar la base de datos" 
-        };
-    }
+export const findMovieById = async (id) => {
+  try {
+    const movie = await prisma.movies.findUnique({
+      where: { id },
+      omit: { created_at: true, updated_at: true },
+    });
+    if (!movie)
+      return {
+        ok: false,
+        message: "La pelicula que buscas no existe",
+      };
+
+    return {
+      ok: true,
+      content: movie,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: "error al consultar la base de datos",
+    };
+  }
 };
 
 export const createMovie = async (payload, fileBuffer) => {
-    try {
-        
-        console.log("aqui entra")
-        if(!payload) return{
-            ok:false,
-            message: "No se ha podido añadir la pelicula"
-        }
-        let imageUrl = null;
-        
-        if(fileBuffer){
-            imageUrl = await uploadloudinary(fileBuffer, "movies")
-        }  
-        const newMovie = await prisma.movies.create({data:{
-            ...payload, movie_image: imageUrl
-                }
-    })
+  try {
+    if (!payload)
+      return {
+        ok: false,
+        message: "No se ha podido añadir la pelicula",
+      };
+    let imageUrl = null;
 
-        return {
-            ok: true,
-            content: newMovie
-        }
-    } catch (error) {
-        return {
-            ok: false,
-            message: "No se ha podido añadir la pelicula"
-        }
+    if (fileBuffer) {
+      imageUrl = await uploadloudinary(fileBuffer, "movies");
     }
+    const newMovie = await prisma.movies.create({
+      data: {
+        ...payload,
+        movie_image: imageUrl,
+      },
+    });
+
+    return {
+      ok: true,
+      content: newMovie,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: "No se ha podido añadir la pelicula",
+    };
+  }
 };
 
-export const updateMovie = async (id, data) =>{
-    try {
-        const update = await prisma.movies.update({
-            where: {id},
-            data
-        })
-        return {
-        ok: true,
-        content: update
-        }
-    } catch (error) {
-        return {
-            ok: false,
-            message: "Error al actualizar la pelicula"
-        }
-    }
-}
+export const updateMovie = async (id, data) => {
+  try {
+    const update = await prisma.movies.update({
+      where: { id },
+      data,
+    });
+    return {
+      ok: true,
+      content: update,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: "Error al actualizar la pelicula",
+    };
+  }
+};
 
-
-export const deleteMovie = async (id) =>{
-    return await prisma.movies.delete({
-        where:{id}
-    })
-}
+export const deleteMovie = async (id) => {
+  return await prisma.movies.delete({
+    where: { id },
+  });
+};
