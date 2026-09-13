b6b729a698019cddf1f1f6f3dddc058c0cfce402
Javi <asuncionjavier96@gmail.com>
2026-09-13 10:09:53 +0200

    refactor: estilos

--- src/controllers/Wishlist.js
+++ src/controllers/Wishlist.js
@@ -1,64 +1,69 @@
-import * as wishlistService from "../services/Wishlist.js"
+import * as wishlistService from "../services/Wishlist.js";
 
-
-export const addWish = async (req,res,next) =>{
-try {
+export const addWish = async (req, res, next) => {
+  try {
     const { movie } = req.body;
-    
-    const {id} = res.locals;
+
+    const { id } = res.locals;
+
     const result = await wishlistService.addToWishlist({
-        movieId: movie,
-        userId: id
-    })
-    if(!result.ok) return next({
+      movieId: movie,
+      userId: id,
+    });
+    if (!result.ok)
+      return next({
         statusCode: 400,
-        message: "No se ha podido añadir la pelicula"
-    })
-    
+        message: "No se ha podido añadir la pelicula",
+      });
+
     return res.status(201).json({
-        success:true,
-        data: "Pelicula añadida a la wishlist"
-    })
-    
-} catch (error) {
-    next(error)
-    }
+      success: true,
+      data: "Pelicula añadida a la wishlist",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getWishlist = async (req,res,next) =>{
-    const {id} = res.locals;
-    
-    const result = await wishlistService.getWishlistbyUser({
-        userId: id
+export const getWishlist = async (req, res, next) => {
+  const { id } = res.locals;
+
+  const result = await wishlistService.getWishlistbyUser({
+    userId: id,
+  });
+
+  if (!result.ok)
+    return next({
+      statusCode: 400,
+      message: "No se ha podido traer la wishlist",
     });
 
-    if(!result.ok) return  next({
+  return res.status(200).json({
+    success: true,
+    message: "wishlist",
+    data: result.data,
+  });
+};
+
+export const deleteWish = async (req, res, next) => {
+  try {
+    const { movieId } = req.params;
+
+    const { id } = res.locals;
+
+    const numericMovie = Number(movieId);
+    if (isNaN(numericMovie)) {
+      return next({
         statusCode: 400,
-        message: "No se ha podido traer la wishlist"
-    })
-
-    
-    return res.status(200).json({
-            success: true,
-            message: "wishlist",
-            data: result.data,
-        })
-};  
-
-
-export const deleteWish = async (req,res,next) =>{
-    try {
-        const { movieId } = req.params;
-
-        const { id } = res.locals;
-
-
-        const result = await wishlistService.removeFromWishlist(id, movieId)
-        return res.status(200).json({
-            success: true,
-            message: "Eliminado de wishlist"
-        })
-    } catch (error) {
-        next(error)
+        message: "El ID de la película debe ser un número válido",
+      });
     }
+    const result = await wishlistService.removeFromWishlist(id, numericMovie);
+    return res.status(200).json({
+      success: true,
+      message: "Eliminado de wishlist",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -1,25 +1,23 @@
-import* as checkoutService from "../services/checkout.js"
+import * as checkoutService from "../services/checkout.js";
 
 export const checkoutCart = async (req, res, next) => {
-    try {
-        const {id} = res.locals;
+  try {
+    const { id } = res.locals;
 
-        const checkCart = await checkoutService.checkoutUserCart(id)
+    const checkCart = await checkoutService.checkoutUserCart(id);
 
-        if(!checkCart){
-            return next ({
-                statusCode: 400,
-                message: "No hay peliculas en el carrito para pasar al checkout"
-            })
-        };
-        res.status(200).json({
-            success: true,
-            data: checkCart.content,
-            message: "Checkout completado"
-        })
-        
-    } catch (error) {
-        next(error)
+    if (!checkCart) {
+      return next({
+        statusCode: 400,
+        message: "No hay peliculas en el carrito para pasar al checkout",
+      });
     }
-
-}
+    res.status(200).json({
+      success: true,
+      data: checkCart.content,
+      message: "Checkout completado",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
